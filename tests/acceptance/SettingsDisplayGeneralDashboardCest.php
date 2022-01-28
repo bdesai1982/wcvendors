7894b68baf7d92e377991ab2eb70f7c689ed4de4
bdesai1982 <77658351+bdesai1982@users.noreply.github.com>
2022-01-28 18:36:57 +0530

    Deleted old scripts which were not working for the current flow

--- tests/acceptance/SettingsDisplayGeneralDashboardCest.php
+++ tests/acceptance/SettingsDisplayGeneralDashboardCest.php
@@ -11,11 +11,11 @@ class SettingsDisplayGeneralDashboardCest
 		$I->amOnPage('/wp-admin/admin.php?page=wcv-settings&tab=display');
 		$I->waitForText('This sets the page used to display the front end vendor dashboard. This page should contain the following shortcode. [wcv_vendor_dashboard]', 200);
 		$I->click('#select2-wcvendors_vendor_dashboard_page_id-container');
-		$I->fillField('body > span > span > span.select2-search.select2-search--dropdown > input', "my account");
+		$I->fillField('body > span > span > span.select2-search.select2-search--dropdown > input', "Vendor Dashboard");
 		$I->wait(1);
 		//$I->click('.select2-results__option select2-results__option--highlighted');//select2-results__option select2-results__option--highlighted
 		$I->pressKey('body > span > span > span.select2-search.select2-search--dropdown > input', \Facebook\WebDriver\WebDriverKeys::ENTER);
-		$I->scrollTo('#mainform > table:nth-child(11) > tbody > tr:nth-child(5) > th > label');
+		$I->scrollTo('#mainform > p.submit > button');
 		$I->click('Save changes');
 		$I->waitForText('Your settings have been saved.', 300);
     }
@@ -34,6 +34,7 @@ class SettingsDisplayGeneralDashboardCest
 		
 		//Restoring default settings.
 		$I->amOnPage('/my-account');//Navigation to accounts page to log out.
+		$I->click('Log out');
 		$I->fillField('#username', 'admin');
 		$I->fillField('#password', '123456');
 		$I->click('Log in');
@@ -44,8 +45,8 @@ class SettingsDisplayGeneralDashboardCest
 		$I->wait(1);
 		//$I->click('.select2-results__option select2-results__option--highlighted');
 		$I->pressKey('body > span > span > span.select2-search.select2-search--dropdown > input', \Facebook\WebDriver\WebDriverKeys::ENTER);
-		$I->scrollTo('#mainform > table:nth-child(11) > tbody > tr:nth-child(5) > th > label');
+		$I->scrollTo('#mainform > p.submit > button');
 		$I->click('Save changes');
-		$I->waitForText('Your settings have been saved.', 300);
+		$I->waitForText('Your settings have been saved.', 300); 
     }
 }
